e4d29c8fecc48df6f9b9e852186d1230171c6982
Andrew <andrewoculus@gmail.com>
2023-03-29 23:51:56 +0300

    Add files via upload

--- main.c
+++ main.c
@@ -3,10 +3,18 @@
 
 int main(){
 
-	int rank;
+	int rank, size;
 	MPI_Init(NULL, NULL);
 	MPI_Comm_rank( MPI_COMM_WORLD, &rank);
+	MPI_Comm_size( MPI_COMM_WORLD, &size);
 	printf("rank: %d\n", rank);
+
+	if( rank == 0 ){
+
+		FILE *fd = fopen( "output.csv", "w" );
+		fprintf( fd,"%d", size);
+		fclose( fd );
+	}
 
 	MPI_Finalize();
 
