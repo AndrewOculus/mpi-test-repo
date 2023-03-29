51b788165b6485421f2723be8fc92d072fcf0d19
Andrew <andrewoculus@gmail.com>
2023-03-29 23:37:38 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <mpi.h>
+
+int main(){
+
+	int rank;
+	MPI_Init(NULL, NULL);
+	MPI_Comm_rank( MPI_COMM_WORLD, &rank);
+	printf("rank: %d\n", rank);
+
+	MPI_Finalaize();
+
+
+}
